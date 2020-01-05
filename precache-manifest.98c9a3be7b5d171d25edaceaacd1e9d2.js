71d87a6bf6de5325f5288b2144150eb1f742b021
Roneet Kumar <roneetparida@gmail.com>
2020-01-05 11:36:54 -0500

    Updates

---
+++ precache-manifest.98c9a3be7b5d171d25edaceaacd1e9d2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "819d3a9d943f46d6bf3ad11a5fea8f54",
+    "url": "/whatsapp-demo/index.html"
+  },
+  {
+    "revision": "7840a73397b47f3f9d48",
+    "url": "/whatsapp-demo/static/css/main.4ae32a29.chunk.css"
+  },
+  {
+    "revision": "58f6d9fd059d502b6d17",
+    "url": "/whatsapp-demo/static/js/2.cfa7844c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/whatsapp-demo/static/js/2.cfa7844c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7840a73397b47f3f9d48",
+    "url": "/whatsapp-demo/static/js/main.e9f948fe.chunk.js"
+  },
+  {
+    "revision": "07ad8374c91cb38b2d2b",
+    "url": "/whatsapp-demo/static/js/runtime-main.41a01dcf.js"
+  },
+  {
+    "revision": "cb49181ecb986163f5f898f9c2c47539",
+    "url": "/whatsapp-demo/static/media/attach.cb49181e.svg"
+  },
+  {
+    "revision": "d9ac88891ef8c3485003afe40095f819",
+    "url": "/whatsapp-demo/static/media/emoji.d9ac8889.svg"
+  },
+  {
+    "revision": "57fbd7e3b3f16072ecb1e9b779df83dd",
+    "url": "/whatsapp-demo/static/media/mac.57fbd7e3.svg"
+  },
+  {
+    "revision": "db2457110c3e8817046f33cc430431a6",
+    "url": "/whatsapp-demo/static/media/menu.db245711.svg"
+  },
+  {
+    "revision": "f59ccbb6ce7efface40b0a4ce57b566a",
+    "url": "/whatsapp-demo/static/media/message.f59ccbb6.svg"
+  },
+  {
+    "revision": "97b76eba86fafcaf8f704622e979ea63",
+    "url": "/whatsapp-demo/static/media/mic.97b76eba.svg"
+  },
+  {
+    "revision": "a87eca9ea0f39475f2fc7613a3af312f",
+    "url": "/whatsapp-demo/static/media/search.a87eca9e.svg"
+  },
+  {
+    "revision": "9f2ba24c7e08bff7390b8eb76ba56830",
+    "url": "/whatsapp-demo/static/media/status.9f2ba24c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/notified/precache-manifest.862aea68282c74b409429a58bacfc1ee.js"
+  "/whatsapp-demo/precache-manifest.98c9a3be7b5d171d25edaceaacd1e9d2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/notified/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/whatsapp-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
