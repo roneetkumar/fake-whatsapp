11ab5778754eab8dac9c0bba328e4a14d62c4e2a
Roneet Kumar <roneetparida@gmail.com>
2020-03-16 22:20:58 -0400

    Updates

---
+++ precache-manifest.0f1c7ba987e6e129aa584b6587037d5f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c83f7375704bace8cc87664470c8b854",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a4f888e32d787c0449d",
+    "url": "/static/css/main.e4a4a01d.chunk.css"
+  },
+  {
+    "revision": "27fd5ae528c89c6d3c64",
+    "url": "/static/js/2.3bf88ad0.chunk.js"
+  },
+  {
+    "revision": "2e7e5a466ae113f07513391c0542446b",
+    "url": "/static/js/2.3bf88ad0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a4f888e32d787c0449d",
+    "url": "/static/js/main.4981109f.chunk.js"
+  },
+  {
+    "revision": "b12bba1ac270c7378e9a",
+    "url": "/static/js/runtime-main.fafdf462.js"
+  },
+  {
+    "revision": "cb49181ecb986163f5f898f9c2c47539",
+    "url": "/static/media/attach.cb49181e.svg"
+  },
+  {
+    "revision": "d9ac88891ef8c3485003afe40095f819",
+    "url": "/static/media/emoji.d9ac8889.svg"
+  },
+  {
+    "revision": "57fbd7e3b3f16072ecb1e9b779df83dd",
+    "url": "/static/media/mac.57fbd7e3.svg"
+  },
+  {
+    "revision": "db2457110c3e8817046f33cc430431a6",
+    "url": "/static/media/menu.db245711.svg"
+  },
+  {
+    "revision": "f59ccbb6ce7efface40b0a4ce57b566a",
+    "url": "/static/media/message.f59ccbb6.svg"
+  },
+  {
+    "revision": "97b76eba86fafcaf8f704622e979ea63",
+    "url": "/static/media/mic.97b76eba.svg"
+  },
+  {
+    "revision": "a87eca9ea0f39475f2fc7613a3af312f",
+    "url": "/static/media/search.a87eca9e.svg"
+  },
+  {
+    "revision": "9f2ba24c7e08bff7390b8eb76ba56830",
+    "url": "/static/media/status.9f2ba24c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/whatsapp-demo/precache-manifest.a274f532b541fc941ece0eab387f5677.js"
+  "/precache-manifest.0f1c7ba987e6e129aa584b6587037d5f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/whatsapp-demo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
